bdbb59affc5e62c6fa79f643b52917574409e7ed
Marco Giordano <{ID}+{username}@users.noreply.github.com>
2024-10-26 02:23:38 +0200

    [ADD] Sweeps

--- a_train_export.py
+++ a_train_export.py
@@ -1,6 +1,13 @@
-import torch 
-from ultralytics import YOLO 
+import torch
+from ultralytics import YOLO
 import wandb
+import argparse
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='Train and export YOLO model.')
+parser.add_argument('--data', type=str, default='coco.yaml', help='Dataset configuration file')
+parser.add_argument('--model', type=str, default='tinyissimo-v8-n', help='Model configuration file or name')
+args = parser.parse_args()
 
 load = False
 exp_id = 'exp1'
@@ -16,16 +23,24 @@ if version == 'v1':
 device = torch.device("cuda")
 if load:
     model_name = f'./results/{exp_id}/weights/last.pt'
-    model = YOLO(model_name) 
+    model = YOLO(model_name)
 else:
-    model_name = f"./ultralytics/cfg/models/tinyissimo/tinyissimo-{version}.yaml"
-    model = YOLO(model_name) 
+    model_name = f"./ultralytics/cfg/models/tinyissimo/{args.model}.yaml"
+    model = YOLO(model_name)
 
 img_size = 256
-input_size = (1, 1, img_size, img_size)  
- 
+input_size = (1, 1, img_size, img_size)
+
+# Must fit in 24GB RAM
+if args.model == 'tinyissimo-v8-b' or args.model == 'tinyissimo-v8-n' or args.model == 'tinyissimo-v8-s':
+    batch_size = 512
+elif args.model == 'tinyissimo-v8-m' or args.model == 'tinyissimo-v8-l':
+    batch_size = 256
+elif args.model == 'tinyissimo-v8-x':
+    batch_size = 128
+
 # Train
-model.train(data="coco.yaml",  project="results", name="exp", optimizer='SGD',  imgsz=img_size,  epochs=1000,  batch=512)
+model.train(data=f"{args.data}.yaml", project="results", name="exp", optimizer='SGD', imgsz=img_size, epochs=1000, batch=batch_size)
 
 # Export
-model.export(format="onnx", project="results", name="exp", imgsz=[img_size,img_size]) 
+model.export(format="onnx", project="results", name="exp", imgsz=[img_size, img_size])
