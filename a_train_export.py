5083fedc00b915b48b61d0c35e70d771316a12c9
Marco Giordano <{ID}+{username}@users.noreply.github.com>
2024-10-29 15:12:48 +0100

    [MOD] Better support multi dataset

--- a_train_export.py
+++ a_train_export.py
@@ -11,24 +11,10 @@ args = parser.parse_args()
 
 wandb.init(project='results')
 
-load = False
-exp_id = 'exp1'
-
-version = 'v8'
-
-if version == 'v1':
-    print('Please, check to modify ultralytics/nn/modules/head/Detect')
-    print('for TinyissimoYOLOv1.3 small and big change')
-    print('line 36 to: self.reg_max=16')
-    exit(1)
-
 device = torch.device("cuda")
-if load:
-    model_name = f'./results/{exp_id}/weights/last.pt'
-    model = YOLO(model_name)
-else:
-    model_name = f"./ultralytics/cfg/models/tinyissimo/{args.model}.yaml"
-    model = YOLO(model_name)
+
+model_name = f"./ultralytics/cfg/models/tinyissimo/{args.model}.yaml"
+model = YOLO(model_name)
 
 img_size = 256
 input_size = (1, 1, img_size, img_size)
@@ -42,14 +28,15 @@ elif args.model == 'tinyissimo-v8-x':
     batch_size = 128
 
 # Train - WARNING! Overwrites existing trained models
-model.train(data=f"{args.data}.yaml", project="results", name=args.model, optimizer='SGD', imgsz=img_size, epochs=1000, batch=batch_size, exist_ok=True)
+folder_name = f"{args.model}_{args.data}"
+model.train(data=f"{args.data}.yaml", project="results", name=folder_name, optimizer='SGD', imgsz=img_size, epochs=5, batch=batch_size, exist_ok=True)
 
 # Export
-model.export(format="onnx", project="results", name="exp", imgsz=[img_size, img_size])
+model.export(format="onnx", project="results", name=folder_name, imgsz=[img_size, img_size], exist_ok=True)
 
 # Validate
-onnx_model = YOLO(f"./results/{args.model}/weights/best.onnx")
-val_metrics = onnx_model.val(data=f"{args.data}.yaml", project="results", name=args.model, task='detect', imgsz=img_size, batch=batch_size)
+onnx_model = YOLO(f"./results/{folder_name}/weights/best.onnx")
+val_metrics = onnx_model.val(data=f"{args.data}.yaml", project="results", name=folder_name, task='detect', imgsz=img_size, batch=batch_size, exist_ok=True)
 
 val_dict = {f"val_{key}": value for key, value in val_metrics.results_dict.items()}
 
